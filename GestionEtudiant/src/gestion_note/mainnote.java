3e424bf8eaf6273683a26d398d68658a48262f80
dream <dream@192.168.56.1>
2022-04-15 18:44:10 +0200

    partie 1,2 et 3

--- GestionEtudiant/src/gestion_note/mainnote.java
+++ GestionEtudiant/src/gestion_note/mainnote.java
@@ -7,6 +7,9 @@ public class mainnote {
 		e1=new Etudiant(1,"hamid");
 		e2=new Etudiant(2,"driss");
 		e3=new Etudiant(3,"youssef");
+		e1.setnote(15);
+		e2.setnote(12);
+		e3.setnote(16);
 		
 		
 
